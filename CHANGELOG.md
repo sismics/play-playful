54601b7a345de5f3197eff355a614906ef4653f7
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2019-03-27 15:06:41 +0100

    Add request utils

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
 ## Change Log
+### Version 1.3.0 (2019-03-27)
+- Add HTTP request utils
 ### Version 1.2.0 (2018-12-14)
 - Add methods to REST functional test
 ### Version 1.1.0 (2018-09-13)
---
+++ app/helpers/request/RequestUtil.java
@@ -0,0 +1,18 @@
+package helpers.request;
+
+import play.mvc.Http;
+
+import java.util.List;
+
+/**
+ * @author jtremeaux
+ */
+public class RequestUtil {
+    public static String getFirstAcceptLanguage() {
+        List<String> acceptLanguage = Http.Request.current().acceptLanguage();
+        if (acceptLanguage == null || acceptLanguage.isEmpty()) {
+            return null;
+        }
+        return acceptLanguage.iterator().next();
+    }
+}
